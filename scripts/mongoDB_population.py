fad0e25e8a7970391eca185dc589f8e42f06225a
Rijndael99 <mazzuoli.yuri@gmail.com>
2020-12-15 18:40:51 +0100

    update scripts

---
+++ scripts/mongoDB_population.py
@@ -0,0 +1,10 @@
+import os
+os.system("mongo < wipe_mongoDB.js")
+os.system("mongoimport  --jsonArray --db StockSim --collection stocks --file stocks_coll.json")
+#os.system("mongoimport -h=172.16.3.94:27017 --jsonArray --db StokSim --collection stocks --file stocks_coll.json")
+
+# usage:
+# run in the same folder of the js script and the colllection file. 
+# to get  the collection file:
+# - if you have the separated jsons, just run the merge script in the right folder
+# - if you don't have json files, just run the fetch script
---
+++ scripts/wipe_mongoDB.js
@@ -0,0 +1,6 @@
+use StockSim;
+db.dropDatabase();
+use StockSim;
+db.createCollection("users");
+db.createCollection("admins");
+exit
