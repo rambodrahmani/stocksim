b25030c3ae0c88a91e74c281eb6b614271d5212b
Rijndael99 <mazzuoli.yuri@gmail.com>
2020-12-17 17:38:34 +0100

    Update wipe_mongoDB.js

--- scripts/wipe_mongoDB.js
+++ scripts/wipe_mongoDB.js
@@ -2,6 +2,6 @@ use StockSim;
 db.dropDatabase();
 use StockSim;
 db.createCollection("users");
-db.users.createIndex({ "username": 1, "portfolios.name": 1 })
+db.users.createIndex({ "username": 1, "portfolios.name": 1 });
 db.createCollection("admins");
 exit
