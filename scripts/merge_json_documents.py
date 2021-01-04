cb4cbe64d148681dbbd8e2f2eef0eb8c61a1a104
Rambod Rahmani <rambodrahmani@autistici.org>
2021-01-04 20:15:31 +0100

    Move city to the embedded document location.

--- scripts/merge_json_documents.py
+++ scripts/merge_json_documents.py
@@ -40,19 +40,19 @@ for json_file in files:
     }
 
     # optional fields: might or might not be present in the original json file
-    try_fetch_attr(ticket_object,estractedFields, "marketCap")
-    try_fetch_attr(ticket_object,estractedFields, "trailingPE")
-    try_fetch_attr(ticket_object,estractedFields, "sector")
-    try_fetch_attr(ticket_object,estractedFields, "city")
-    try_fetch_attr(ticket_object,estractedFields, "website")
-    try_fetch_attr(ticket_object,estractedFields, "industry")
-    try_fetch_attr(ticket_object,estractedFields, "longBusinessSummary")
+    try_fetch_attr(ticket_object, estractedFields, "marketCap")
+    try_fetch_attr(ticket_object, estractedFields, "trailingPE")
+    try_fetch_attr(ticket_object, estractedFields, "sector")
+    try_fetch_attr(ticket_object, estractedFields, "website")
+    try_fetch_attr(ticket_object, estractedFields, "industry")
+    try_fetch_attr(ticket_object, estractedFields, "longBusinessSummary")
 
     # location enbedded document
     location={}
-    try_fetch_attr(ticket_object,location, "state")
-    try_fetch_attr(ticket_object,location, "country")
-    try_fetch_attr(ticket_object,location, "phone")
+    try_fetch_attr(ticket_object, location, "city")
+    try_fetch_attr(ticket_object, location, "state")
+    try_fetch_attr(ticket_object, location, "country")
+    try_fetch_attr(ticket_object, location, "phone")
 
     # compose address field
     try:
@@ -69,7 +69,7 @@ for json_file in files:
 
 # write collection in the new file
 with open("stocks.json", "w") as target_file:
-   json.dump(ticker_collection,target_file)
+   json.dump(ticker_collection, target_file)
 
 #os.system("mongoimport -h=172.16.3.94:27017 --jsonArray --db StokSim --collection stocks --file stocks_coll.json").
 #os.system("mongoimport  --jsonArray --db StokSim --collection stocks --file stocks_coll.json").
